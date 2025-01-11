a856c4e6ea9ff2455501ebe90fb15f149406ec05
Jonathan Maes <jonathan145b@hotmail.com>
2025-01-11 14:16:30 +0100

    Allow credentials for multiple WIFI networks

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 This repository contains [MicroPython](https://micropython.org/) source code for a [Raspberry Pico W](https://www.raspberrypi.com/documentation/microcontrollers/pico-series.html#picow-technical-specification)-controlled clock counting down to the next launch, leveraging the [LL2 API](https://thespacedevs.com/llapi).
 
 > [!NOTE]  
-> This requires access to a WiFi network: create a file `wlan.json` (in the same directory as `main.py`) to store the name and password of your WiFi network in the format `{"ssid": "<network_name>", "password": "..."}`.
+> This requires access to a WiFi network: create a file `wlan.json` (in the same directory as `main.py`) to store the name and password of your WiFi network(s) in the format `[{"ssid": ..., "password": ...}, ...]`.
--- web.py
+++ web.py
@@ -2,35 +2,112 @@ import errno
 import json
 import network
 import ntptime
+import requests
 import time
 from machine import Pin
 
-def connect(): # Expects wlan.json to contain {"ssid": ..., "password": ...}
+
+def get_credentials(file: str = "wlan.json"):
+    """ Load WiFi credentials from a JSON file.
+        The JSON file can contain either:
+            a single object    {"ssid": "wifi name", "password": "..."}
+            a list of objects  [{"ssid": ..., "password": ...}, ...]
+    """
+    with open(file, "r") as w:
+        cred = json.load(w)
+    if not isinstance(cred, list):
+        cred = [cred]
+    return {c["ssid"]: c.get("password", None) for c in cred}
+
+
+def internet_check():
+    """ Check if an internet connection is available by performing a simple HTTP request. """
+    try:
+        response = requests.get("http://www.google.com")
+        response.close()
+        return True
+    except:
+        return False
+
+
+def connect(threshold_db: int = 100, credfile: str = "wlan.json", timeout: float = 10.) -> str|None:
+    """ Connect to the strongest available WiFi network based on signal strength.
+
+        Args:
+            threshold_db (int): Signal strength difference threshold in dB to consider switching.
+                Default is 100 to prevent switching. A reasonable value to allow switching would be 20,
+                but note that switching can cause loops when calling connect() multiple times.
+            credfile (str): Path to the JSON file containing WiFi credentials.
+            timeout (float): Number of seconds to attempt connecting to each WiFi network.
+
+        Returns:
+            str|None: IP if connected to a network, None otherwise.
+    """
     pico_led = Pin("LED", Pin.OUT)
     wlan = network.WLAN(network.STA_IF)
-    if wlan.isconnected():
-        print("WIFI already connected.")
-        return
     wlan.active(True)
-    with open("wlan.json", "r") as w:
-        cred = json.load(w)
-        wlan.connect(cred["ssid"], cred["password"]) # WIFI network name and password
-    while wlan.isconnected() == False:
-        for _ in range(10): # Blink onboard LED while not connected to WLAN
+
+    credentials = get_credentials(credfile)
+    available_networks = wlan.scan()
+    
+    # Put networks that require authentication (bool(x[4])) first, then sort by signal strength (RSSI = x[3]).
+    # This way, we first try networks that require passwords which we might have in <credfile>, and only then
+    # do we try open networks (because they often don't work as they require some form of login anyway).
+    available_networks.sort(key=lambda x: (bool(x[4]), x[3]), reverse=True)
+    current_rssi = wlan.status('rssi') if wlan.isconnected() else -100
+    print(*[[nw[3], nw[0]] for nw in available_networks], current_rssi, sep="\n")
+
+    for nw in available_networks:
+        ssid = nw[0].decode("utf-8")
+        rssi = nw[3]
+        authmode = nw[4]
+
+        if wlan.isconnected() and rssi <= current_rssi + abs(threshold_db):
+            continue # Skip weaker networks if current connection is strong enough
+
+        if authmode == 0: # Open network
+            wlan.connect(ssid)
+        elif ssid in credentials: # Secured network with known credentials
+            wlan.connect(ssid, credentials[ssid])
+        else:
+            continue
+
+        # Wait for connection
+        dt = 0.2
+        for j in range(int(timeout/dt)): # Wait up to 10 seconds
+            if wlan.isconnected():
+                if not internet_check() and authmode == 0:
+                    print(f'Connected to {ssid}, but no internet access.')
+                    wlan.disconnect()
+                    break
+                else: # This is mostly for open networks: check if they actually work.
+                    ip = wlan.ifconfig()[0]
+                    print(f'Connected to {ssid} with IP {ip}')
+                    pico_led.on()
+                    setUTCtime()
+                    pico_led.off()
+                    return ip
             pico_led.toggle()
-            time.sleep(0.2)
-    ip = wlan.ifconfig()[0]
-    print(f'Connected on {ip}')
-    pico_led.on()
-    setUTCtime()
-    pico_led.off()
-    return ip
+            time.sleep(dt)
+
+    if wlan.isconnected():
+        print(f"Already connected to sufficiently strong WIFI network {wlan.config('ssid')}.")
+        ip = wlan.ifconfig()[0]
+        return ip
+    else:
+        print("No suitable networks found or connection failed.")
+        return None
 
 def setUTCtime():
     try: # Fetch and set the correct UTC time
         ntptime.settime()
     except Exception as e:
-        if e.errno != errno.ETIMEDOUT: raise e
+        if e.errno != errno.ETIMEDOUT:
+            print(e)
 
 if __name__ == "__main__":
-    connect()
+    try:
+        for _ in range(1000):
+            connect()
+    except KeyboardInterrupt:
+        pass
