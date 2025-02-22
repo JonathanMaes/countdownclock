JonathanMaes/countdownclock

commit b6452cd5c9326cb5a9d8839dc91b9107b6e35d3d

Move BMP, PNG, JPG imports

lcd.py
@@ -2,6 +2,10 @@
 import framebuf
 from machine import Pin, SPI, PWM
 
+from bmp_file_reader import BMPFileReader
+from JPEGdecoder import jpeg
+from PNGdecoder import png
+
 
 class LCD_1inch8(framebuf.FrameBuffer):
     def __init__(self, brightness=1, freq=10_000_000, BLpin=13, DCpin=8, RSTpin=12, MOSIpin=11, SCKpin=10, CSpin=9):
@@ -145,18 +149,15 @@ class LCD_1inch8(framebuf.FrameBuffer):
             self.pixel(x, y, self.color(r, g, b))
     
     def show_image_BMP(self, x, y, file_handle): # Takes a file object, not a path!
-        from bmp_file_reader import BMPFileReader
         reader = BMPFileReader(file_handle)
         for row_i in range(0, reader.get_height()):
             for col_i, color in enumerate(reader.get_row(row_i)):
                 self.set_pixel(x + col_i, y + row_i, (color.red << 16 | color.green << 8 | color.blue))
     
     def show_image_PNG(self, x, y, file_handle): # Takes a file object or path.
-        from PNGdecoder import png
         png(file_handle, callback=self.set_pixel, fastalpha=False).render(x, y)
     
     def show_image_JPG(self, x, y, file_handle): # Takes a file object or path.
-        from JPEGdecoder import jpeg
         jpeg(file_handle, callback=self.set_pixel).render(x, y)
         
 

commit 150fa80983b8f5e5b3bff4235218a43bd5429417

Undo b6452cd5 which caused excessive memory usage

lcd.py
@@ -1,10 +1,7 @@
 # Adapted from pico-LCD-1.8.py provided by WaveShare in https://www.waveshare.com/wiki/Pico-LCD-1.8#Examples
 import framebuf
+import gc
 from machine import Pin, SPI, PWM
-
-from bmp_file_reader import BMPFileReader
-from JPEGdecoder import jpeg
-from PNGdecoder import png
 
 
 class LCD_1inch8(framebuf.FrameBuffer):
@@ -149,16 +146,25 @@ class LCD_1inch8(framebuf.FrameBuffer):
             self.pixel(x, y, self.color(r, g, b))
     
     def show_image_BMP(self, x, y, file_handle): # Takes a file object, not a path!
+        from bmp_file_reader import BMPFileReader # Only import when necessary
         reader = BMPFileReader(file_handle)
         for row_i in range(0, reader.get_height()):
             for col_i, color in enumerate(reader.get_row(row_i)):
                 self.set_pixel(x + col_i, y + row_i, (color.red << 16 | color.green << 8 | color.blue))
+        del BMPFileReader
+        gc.collect()
     
     def show_image_PNG(self, x, y, file_handle): # Takes a file object or path.
+        from PNGdecoder import png # Only import when necessary
         png(file_handle, callback=self.set_pixel, fastalpha=False).render(x, y)
+        del png
+        gc.collect()
     
     def show_image_JPG(self, x, y, file_handle): # Takes a file object or path.
+        from JPEGdecoder import jpeg # Only import when necessary
         jpeg(file_handle, callback=self.set_pixel).render(x, y)
+        del jpeg
+        gc.collect()
         
 
 if __name__ == "__main__":